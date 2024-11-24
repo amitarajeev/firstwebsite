396c33f4cf39f7f4efb3cf8743e7cb89806429dc
amitarajeev <ammuasha12@gmail.com>
2024-11-24 22:25:01 +1100

    Adding buttons and making them interactive

--- public/script.js
+++ public/script.js
@@ -1,10 +1,10 @@
-// Greet the user
-console.log("Welcome to My First Website!");
 
-// Add a button interaction
-document.addEventListener("DOMContentLoaded", () => {
-    const header = document.querySelector("header");
-    header.addEventListener("click", () => {
-        alert("You clicked the header!");
-    });
-});
+// Function to show an alert message
+function alertMessage() {
+  alert("Hello dear! Nice to meet you.");
+}
+
+// Function to navigate to the 'About Me' section
+function goToAbout() {
+  window.location.hash = "about";
+}
