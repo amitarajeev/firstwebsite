03a66e8050b1cb0762df52aeed719018d39372e6
amitarajeev <ammuasha12@gmail.com>
2024-11-24 23:38:29 +1100

    Adding different sections: Home, About me, Projects and Contact

--- public/script.js
+++ public/script.js
@@ -1,10 +1,11 @@
 
 // Function to show an alert message
 function alertMessage() {
-  alert("Hello dear! Nice to meet you.");
+  alert("Hello! Nice to meet you.");
 }
 
 // Function to navigate to the 'About Me' section
 function goToAbout() {
   window.location.hash = "about";
 }
+
